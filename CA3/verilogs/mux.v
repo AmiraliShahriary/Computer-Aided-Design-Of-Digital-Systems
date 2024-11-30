3d61c617f7f20ef1f6ecf5ec459bfca3c8056835
matahho <mata1381@gmail.com>
2024-11-30 10:43:01 +0330

    mux added

---
+++ CA3/verilogs/mux.v
@@ -0,0 +1,20 @@
+module multiplexer #(parameter WIDTH = 8) (input wire [WIDTH-1:0] in0,input wire [WIDTH-1:0] in1,input wire sel,output wire [WIDTH-1:0] out);
+
+genvar i;
+generate
+    for (i = 0; i < WIDTH; i = i + 1) begin : mux_block
+        C1 mux_inst (
+            .A0(in0[i]),
+            .A1(in1[i]),
+            .SA(sel),
+            .B0(1'b1),
+            .B1(1'b1),
+            .SB(1'b1),
+            .S0(1'b0),
+            .S1(1'b0),
+            .F(out[i])
+        );
+    end
+endgenerate
+
+endmodule
